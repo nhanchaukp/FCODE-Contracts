a5c1342c8635500232532dc2870b072ce5836ca3
nhanchaukp <ctnhan044@gmail.com>
2025-04-03 13:22:12 +0700

    ignore

--- contracts/FCODE.sol
+++ contracts/FCODE.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FCODE is ERC20, Ownable {
     uint256 public balance;
